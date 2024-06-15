be01cd7c67b9dbf30390b6ec72c573d1d0646537
SardarAndimeh <36482913+SardarAndimeh@users.noreply.github.com>
2024-06-15 15:24:01 +0200

    feat: add basic calculation (#2)
    * feat: add operation
    
    * feat: sub operation
    
    * feat: multiplication & division operations
    
    * fix: Update main.go
    
    Co-authored-by: Siamand <9437465+code-by-sia@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Siamand <9437465+code-by-sia@users.noreply.github.com>

--- main.go
+++ main.go
@@ -6,3 +6,19 @@ func main() {
 
 	fmt.Println("Calculator v1.0")
 }
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func subtract(x int, y int) int {
+	return x - y
+}
+
+func multiply(x int, y int) int {
+	return x * y
+}
+
+func divide(x int, y int) int {
+	return x / y
+}

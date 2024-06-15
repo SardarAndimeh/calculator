0aac38d4056a89e638ffc752d9588ab5b3708c57
Sardar <sardar.andimeh7@gmail.com>
2024-06-15 15:11:50 +0200

    feat: multiplication & division operations

--- main.go
+++ main.go
@@ -14,3 +14,11 @@ func add(x int, y int) int {
 func sub(x int, y int) int {
 	return x - y
 }
+
+func multiply(x int, y int) int {
+	return x * y
+}
+
+func divide(x int, y int) int {
+	return x / y
+}
